e60b5137500626c0b9c6e704d01472374c451bda
Pavan Kumar Varri <PavanKumar.Varri@pfgc.com>
2022-10-04 00:59:47 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  type = any
+}
+
+variable "cluster_location" {
+  type = any
+}
+
+variable "project_id" {
+  type = any
+}
+variable "project_region" {
+  type = any
+}
